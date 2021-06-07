85bfd41c1305f08ed4f925f7503d3ca6ff570be9
zooroy <zooroy13@gmail.com>
2021-06-07 17:56:04 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,6 @@ const Index = () => {
   const pp = useSelector(state => state);
   // console.log('pp', pp);
 
-  //fixed bugs
-
-  //add layout5
-  // fixed bugs
   return (
     <Layout changeBG={pp.changeBG}>
       <h1>{pp.changeText}</h1>
@@ -19,8 +15,6 @@ const Index = () => {
     </Layout>
   );
 };
-
-//fixed bugs vol.2
 
 // export default connect(state => ({
 //   changeText: state.changeText,
